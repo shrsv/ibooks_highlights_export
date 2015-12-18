be739a39d08b317cbda9cf81f6bc7e169211e166
shrsv <shrijith.sv@gmail.com>
2015-12-18 21:57:43 +0530

    Migrate to Flask template

--- index.py
+++ index.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 import ibooks_highlights_exporter as ib
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     res = ib.get_all_titles()
-    op = ["<ul>"]
+    #op = ["<ul>"]
 
-    for it in res:
-        op.append("<li id='%s'><input type='checkbox' name='titles' value='%s' />%s</li>" % (it["ZASSETID"], it["ZASSETID"], it["ZTITLE"]))
+    #for it in res:
+        #op.append("<li id='%s'><input type='checkbox' name='titles' value='%s' />%s</li>" % (it["ZASSETID"], it["ZASSETID"], it["ZTITLE"]))
 
-    op.append("</ul>")
+    #op.append("</ul>")
 
-    return "".join(op)
+    #return "".join(op)
+    return render_template('index.html', booklist=res)
 
 if __name__ == "__main__":
     app.debug = True
