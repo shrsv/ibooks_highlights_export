b3db1177579681b0fc76b03f035c86baeb31c7c2
shrsv <shrijith.sv@gmail.com>
2015-12-19 23:08:00 +0530

    Implement export button

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import ibooks_highlights_exporter as ib
 app = Flask(__name__)
 
@@ -14,6 +14,10 @@ def hello():
 
     #return "".join(op)
     return render_template('index.html', booklist=res)
+
+@app.route("/export", methods=['POST'])
+def export():
+    return jsonify(request.form)
 
 if __name__ == "__main__":
     app.debug = True
